4432d35b09936dbefd77877acf30ae980ba66c71
magsalayyin2 <yibo.magsalay.coc@phinmaed.com>
2026-08-17 22:22:12 +0800

    Add files via upload

---
+++ Activity2 LE1-Magsalay_Yin.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    printf("=====*******************************************=====\n");
+    printf("+                      WELCOME                      +\n");
+    printf("+              TO COMPUTER PROGRAMMING              +\n");
+
+    printf("+                      LEARN                        +\n");
+    printf("+              Code - Syntax - Debug it!            +\n");
+
+    printf("=====*******************************************=====\n");
+    return 0;
+}
